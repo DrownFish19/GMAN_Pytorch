3243bc2328947ceca81a2469008dd9ce65041f9c
DrowFish19 <DrownFish19@gmail.com>
2023-02-16 14:41:41 +0800

    update for new networkx api

--- METR/node2vec/generateSE.py
+++ METR/node2vec/generateSE.py
@@ -1,7 +1,10 @@
-import node2vec
-import numpy as np
+import pickle
+
 import networkx as nx
+import numpy as np
 from gensim.models import Word2Vec
+
+import node2vec
 
 is_directed = True
 p = 2
@@ -11,9 +14,11 @@ walk_length = 80
 dimensions = 64
 window_size = 10
 iter = 1000
-Adj_file = '../data/Adj(METR).txt'
-SE_file = '../data/SE(METR).txt'
-
+Adj_file = '../data/PEMS04.csv'
+SE_file = '../data/PEMS04-SE.txt'
+node_num = 307
+# id_filename = "../data/PEMS03-id.txt"
+id_filename = None
 
 def read_graph(edgelist):
     G = nx.read_edgelist(
@@ -23,7 +28,7 @@ def read_graph(edgelist):
     return G
 
 
-def get_adjacency_matrix_2direction(distance_df_filename, num_of_vertices):
+def get_adjacency_matrix_2direction(distance_df_filename, num_of_vertices, id_filename=None):
     import csv
 
     A = np.zeros((int(num_of_vertices), int(num_of_vertices)),
@@ -31,6 +36,10 @@ def get_adjacency_matrix_2direction(distance_df_filename, num_of_vertices):
 
     distaneA = np.zeros((int(num_of_vertices), int(num_of_vertices)),
                         dtype=np.float32)
+    if id_filename:
+        with open(id_filename, 'r') as f:
+            id_dict = {int(i): idx for idx, i in enumerate(f.read().strip().split('\n'))}  # 把节点id（idx）映射成从0开始的索引
+
     with open(distance_df_filename, 'r') as f:
         f.readline()
         reader = csv.reader(f)
@@ -38,6 +47,10 @@ def get_adjacency_matrix_2direction(distance_df_filename, num_of_vertices):
             if len(row) != 3:
                 continue
             i, j, distance = int(row[0]), int(row[1]), float(row[2])
+
+            if id_filename:
+                i = id_dict[i]
+                j = id_dict[j]
             A[i, j] = 1
             A[j, i] = 1
             distaneA[i, j] = distance
@@ -55,11 +68,11 @@ def learn_embeddings(walks, dimensions, output_file):
     return
 
 
-adj_np, _ = get_adjacency_matrix_2direction(Adj_file, node_num)
-# output = open('PEMS08_adj_mx.pkl', 'wb')
-# pickle.dump(adj_np, output)
-# output.close()
-nx_G = nx.from_numpy_array(adj_np, nx.DiGraph())
+adj_np, _ = get_adjacency_matrix_2direction(Adj_file, node_num, id_filename=id_filename)
+output = open('PEMS04_adj_mx.pkl', 'wb')
+pickle.dump(adj_np, output)
+output.close()
+nx_G = nx.from_numpy_array(A=adj_np, create_using=nx.DiGraph())
 G = node2vec.Graph(nx_G, is_directed, p, q)
 G.preprocess_transition_probs()
 walks = G.simulate_walks(num_walks, walk_length)
